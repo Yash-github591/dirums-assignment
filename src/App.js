2b5afa34d204402be3f6f5c5075e0ac6e1a1e70b
Yash-github591 <yashgargjsr1999@gmail.com>
2024-02-23 16:57:30 +0530

    assignment for 2nd round

--- src/App.js
+++ src/App.js
@@ -1,166 +1,214 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Product from "./components/Product";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [double, setDouble] = useState(1);
-  const [single, setSingle] = useState(0);
-  const [cols, setCols] = useState(2);
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((dataFromUrl) => setData(dataFromUrl));
-  }, []);
-
-  const handleDouble = () => {
-    const element = document.querySelector(".productTable");
-    if (double === 1) {
-      element.classList.remove("grid-cols-2");
-      element.classList.add("grid-cols-1");
-      setDouble(1 - double);
-      setSingle(1 - single);
-    }
-  };
-  const handleSingle = () => {
-    const element = document.querySelector(".productTable");
-    if (single === 1) {
-      element.classList.remove("grid-cols-1");
-      element.classList.add("grid-cols-2");
-      setDouble(1 - double);
-      setSingle(1 - single);
-    }
-  };
-
   return (
     <div className="App font-mono h-screen w-full">
       <nav>
         <div className="first flex bg-black h-12">
-          <img src="./logos/menu.png" className="lg:hidden  w-7 h-7 m-2"></img>
           <div className="flex justify-center gap-10 w-full">
-            <img src="./logos/dirums.png" className="w-22 h-10"></img>
+            <img src="./images/dirums.png" className="w-22 h-10"></img>
             <input
               type="text"
               name="searchTerm"
               className="bg-gray-300 w-1/2 h-5 my-4"
             ></input>
-            <img src="./logos/wishlist.png" className="w-10 h-5 my-4"></img>
+            <img src="./images/wishlist.png" className="w-10 h-5 my-4"></img>
           </div>
         </div>
-        <div className="second hidden lg:flex justify-center gap-10 h-auto w-full bg-yellow-600  py-1 text-white">
-          <h2>Artwork for Wall</h2>
-          <h2>Folk Art & Craft</h2>
-          <h2>Wildlife Paintings</h2>
-          <h2>Home & Living</h2>
-          <h2>Painting for Living Room</h2>
-          <h2>Religious Paintings</h2>
-        </div>
-        <div className="third h-10 flex gap-2 bg-gray-300 px-10 py-2">
-          <h2 className="text-xl">Home</h2>
-          <h1 className="text-xl">{">"}</h1>
-          <h2 className="text-xl text-yellow-600">Artworks</h2>
-        </div>
-      </nav>
-      <div className="flex h-full">
-        <div className="leftbar hidden lg:block h-full w-1/6 relative px-12 py-8">
-          <h1 className="text-2xl font-bold relative">
-            Filters
-            <span className="absolute bottom-0 left-0 h-1 w-auto bg-black"></span>
-          </h1>
-          <h2 className="text-xl mt-8 font-black"> Price</h2>
-          <label className="text-sm">
-            <input type="radio" name="below" className="m-1" />
-            Below 10,000
-            <br />
-          </label>
-          <label className="text-sm">
-            <input type="radio" name="below" className="m-1" />
-            10,000-25,000
-            <br />
-          </label>
-          <label className="text-sm">
-            <input type="radio" name="below" className="m-1" />
-            25,000-50,000
-            <br />
-          </label>
-          <label className="text-sm">
-            <input type="radio" name="below" className="m-1" />
-            50,000-1,00,000
-            <br />
-          </label>
-          <label className="text-sm">
-            <input type="radio" name="below" className="m-1" />
-            Above 1,00,000
-            <br />
-          </label>
-          <h1 className="text-2xl font-bold relative mt-8">Sort By Date</h1>
-          <label className="text-sm">
-            <input type="radio" name="below" className="m-1" />
-            Newest
-            <br />
-          </label>
-          <label className="text-sm">
-            <input type="radio" name="below" className="m-1" />
-            Oldest
-            <br />
-          </label>
-          <h1 className="text-2xl font-bold relative mt-8">Sort By Date</h1>
-          <label className="text-sm">
-            <input type="radio" name="below" className="m-1" />
-            High to Low
-            <br />
-          </label>
-          <label className="text-sm">
-            <input type="radio" name="below" className="m-1" />
-            Low to High
-            <br />
-          </label>
-          <button className="my-10 border-2 rounded-xl border-black p-2">
-            Reset Filters
-          </button>
-        </div>
-        <div className="rightbar w-auto lg:w-5/6">
-          <div className="border-l-2 border-black">
-            <h1 className="text-4xl m-4 mt-0 pt-4 font-extrabold text-center">
-              OUR EXQUISITE ART
-              <br />
-              COLLECTION
-            </h1>
-            <div className=" h-1 w-auto mx-2 bg-yellow-600"></div>
-            <h1 className="text-lg text-center p-4 m-4 font-extrabold">
-              Acquire original art from our online art gallery. Take a look at
-              our vast collection of artwork created by artists from across the
-              world.
-            </h1>
-            <div className=" h-1 w-30 mx-2 bg-yellow-600"></div>
-            <div className="lg:hidden flex buttons justify-end pt-2">
-              <img
-                src={`./buttons/double-${double}.png`}
-                className="DoubleCol h-4 mx-2"
-                onClick={handleDouble}
-              ></img>
-              <img
-                src={`./buttons/single-${single}.png`}
-                className="SingleCol h-4 mr-8"
-                onClick={handleSingle}
-              ></img>
+        <div className="second flex justify-center gap-10 h-auto w-full bg-yellow-600">
+          <div className="group">
+            <h2 className="text-white group-hover:text-yellow-600 hover:bg-gray-300 p-2">
+              Artworks
+            </h2>
+            <div className="opacity-0 scale-0 group-hover:opacity-100 shadow-lg mb-2 group-hover:scale-100 w-1/2 grid grid-cols-3 gap-2 p-4 bg-gray-100 text-black absolute">
+              <div className="column1">
+                <h3 className="font-bold m-2 text-lg">Paintings</h3>
+                <h3 className="m-2">Acrylic</h3>
+                <h3 className="m-2">WaterColor</h3>
+                <h3 className="m-2">Oil Paintings</h3>
+                <h3 className="m-2">Mix Media Paintings</h3>
+                <h3 className="m-2">Ink on Paper Paintings</h3>
+                <h3 className="m-2">Wildlife Paintings</h3>
+                <h3 className="m-2">Natural Landscape</h3>
+                <h3 className="m-2">Home Office Art</h3>
+              </div>
+              <div className="column2">
+                <h3 className="font-bold m-2 text-lg">Folk Art</h3>
+                <h3 className="m-2">Madhubani Art</h3>
+                <h3 className="m-2">Mandala Art</h3>
+                <h3 className="m-2">Gond Art</h3>
+                <h3 className="m-2">Bhil Art</h3>
+                <h3 className="m-2">Dhokra Art</h3>
+                <h3 className="m-2">Pattachitra Art</h3>
+              </div>
+              <div className="column3">
+                <h3 className="font-bold m-2 text-lg">Religious</h3>
+                <h3 className="m-2">Lord Buddha Paintings</h3>
+                <h3 className="m-2">Lord Krishna Paintings</h3>
+                <h3 className="m-2">Lord Shiva Paintings</h3>
+                <h3 className="m-2">Lord Vishnu Paintings</h3>
+                <h3 className="m-2">Lord Ganesha Paintings</h3>
+                <h3 className="m-2">Lord Hanuman Paintings</h3>
+                <h3 className="m-2">Lord Ram Paintings</h3>
+                <h3 className="m-2">Maa Durga Paintings</h3>
+                <h3 className="m-2">Maa Lakshmi Paintings</h3>
+                <h3 className="m-2">Maa Saraswati Paintings</h3>
+              </div>
             </div>
           </div>
-          <div className="productTable grid grid-cols-2 lg:grid-cols-3 flex flex-wrap border-l-2 border-black p-3 items-center justify-items-center">
-            {data &&
-              data.products.map((currProduct) => (
-                <Product
-                  description={currProduct.description}
-                  title={currProduct.title}
-                  price={currProduct.price}
-                  src={currProduct.images[0]}
-                  key={currProduct.id}
-                />
-              ))}
+          <div className="group">
+            <h2 className="text-white  hover:text-yellow-600 hover:bg-gray-300 p-2">
+              Home & Living
+            </h2>
+            <div className="opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 w-full h-1/2 shadow-lg mb-2 left-0 grid grid-cols-6 flex justify-around gap-2 p-8 bg-gray-100 text-black absolute">
+              <div className="column1">
+                <h3 className="font-bold m-2 text-lg">Wall Decor</h3>
+                <h3 className="m-2">Wall Art & Paintings</h3>
+                <h3 className="m-2">Wall Plates</h3>
+                <h3 className="m-2">Embroidery Hoops</h3>
+                <h3 className="m-2">Photo Frames</h3>
+                <h3 className="font-bold mt-4 mb-2 mx-2 text-lg">
+                  Wall Clocks
+                </h3>
+                <h3 className="m-2">Modern Clocks</h3>
+                <h3 className="m-2">Resin Clocks</h3>
+                <h3 className="m-2">Lippan Clocks</h3>
+              </div>
+              <div className="column2">
+                <h3 className="font-bold m-2 text-lg">Table Decor</h3>
+                <h3 className="m-2">Decorative Trays</h3>
+                <h3 className="m-2">Elegant Coasters</h3>
+                <h3 className="m-2">Dhokra</h3>
+                <h3 className="m-2">Vases</h3>
+                <h3 className="font-bold mt-4 mb-2 mx-2 text-lg">Trays</h3>
+                <h3 className="m-2">Wooden Trays</h3>
+                <h3 className="m-2">Pattachitra Trays</h3>
+                <h3 className="m-2">Resin Trays</h3>
+                <h3 className="m-2">Coasters</h3>
+              </div>
+              <div className="column3">
+                <h3 className="font-bold m-2 text-lg">Resin</h3>
+                <h3 className="m-2">Trays</h3>
+                <h3 className="m-2">Tea Light Holders</h3>
+                <h3 className="m-2">Pooja Thalis</h3>
+                <h3 className="m-2">Key Chains</h3>
+                <h3 className="m-2">Coasters</h3>
+                <h3 className="m-2">Photo Frames</h3>
+                <h3 className="m-2">Clocks</h3>
+              </div>
+              <div className="column4">
+                <h3 className="font-bold m-2 text-lg">Lippan</h3>
+                <h3 className="m-2">Wall Hangings</h3>
+                <h3 className="m-2">Photo Frames</h3>
+                <h3 className="m-2">Tea Light Holders</h3>
+                <h3 className="m-2">Mirror</h3>
+                <h3 className="font-bold mt-4 mb-2 mx-2 text-lg">Candles</h3>
+                <h3 className="m-2">Decorative Candles</h3>
+                <h3 className="m-2">Tea Light Candles</h3>
+              </div>
+              <div className="column5">
+                <h3 className="font-bold m-2 text-lg">Photo Frames</h3>
+                <h3 className="m-2">Modern Frames</h3>
+                <h3 className="m-2">Resin Frames</h3>
+                <h3 className="m-2">Lippan Frames</h3>
+                <h3 className="font-bold mt-4 mb-2 mx-2 text-lg">
+                  Candles Holders
+                </h3>
+                <h3 className="m-2">Resin Candle Holders</h3>
+                <h3 className="m-2">Tea Light Holders</h3>
+              </div>
+              <div className="column6">
+                <h3 className="font-bold m-2 text-lg">Pooja Essentials</h3>
+                <h3 className="m-2">Spiritual Wall Hangings</h3>
+                <h3 className="m-2">Pooja Thalis</h3>
+                <h3 className="m-2">Diya</h3>
+                <h3 className="m-2">Incense Burner</h3>
+                <h3 className="m-2">God Idols</h3>
+                <h3 className="font-bold mt-4 mb-2 mx-2 text-lg">
+                  Decorative Pot & Planters
+                </h3>
+                <h3 className="m-2">Terracotta Planters</h3>
+                <h3 className="m-2">Terracotta Pots</h3>
+              </div>
+            </div>
+          </div>
+          <div className="group">
+            <h2 className="text-white  group-hover:text-yellow-600 group-hover:bg-gray-300 p-2">
+              Jwellery Accessories
+            </h2>
+            <div className="opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 grid grid-cols-2 gap-2 p-2 bg-gray-100 text-black absolute">
+              <div className="column1">
+                <h3 className="font-bold m-2 text-lg">Earrings</h3>
+                <h3 className="m-2">Resin</h3>
+                <h3 className="m-2">Quiling</h3>
+                <h3 className="m-2">Bead & Mirror</h3>
+                <h3 className="m-2">Clay</h3>
+                <h3 className="m-2">Fabric</h3>
+                <h3 className="m-2">Keyrings</h3>
+              </div>
+              <div className="column2">
+                <h3 className="font-bold m-2 text-lg">Jwellery Set</h3>
+                <h3 className="m-2">Resin Jwellery</h3>
+                <h3 className="m-2">Quiling Jwellery</h3>
+                <h3 className="m-2">Sea-Sell Jwellery</h3>
+                <h3 className="m-2">Clay Jwellery</h3>
+                <h3 className="m-2">Fabric Jwellery</h3>
+                <h3 className="m-2">Wood Jwellery</h3>
+                <h3 className="m-2">Kundan Jwellery</h3>
+              </div>
+            </div>
+          </div>
+          <div className="group">
+            <h2 className="text-white group-hover:text-yellow-600 group-hover:bg-gray-300 p-2">
+              Kitchen & Dining
+            </h2>
+            <div className="opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 w-1/3 grid grid-cols-3 gap-2 p-2 bg-gray-100 text-black absolute">
+              <div className="column1">
+                <h3 className="font-bold m-2 text-lg">DrinkWare</h3>
+                <h3 className="m-2">Coffee Mugs</h3>
+                <h3 className="m-2">Water Bottles</h3>
+                <h3 className="m-2">Teacups Set</h3>
+                <h3 className="m-2">Glass Set</h3>
+                <h3 className="m-2">Fabric</h3>
+              </div>
+              <div className="column2">
+                <h3 className="font-bold m-2 text-lg">ServeWare</h3>
+                <h3 className="m-2">Serving Tray</h3>
+                <h3 className="m-2">Tray Set</h3>
+              </div>
+              <div className="column3">
+                <h3 className="font-bold m-2 text-lg">TableWare</h3>
+                <h3 className="m-2">Coasters</h3>
+                <h3 className="m-2">Kettle</h3>
+              </div>
+            </div>
+          </div>
+          <div className="group relative">
+            <h2 className="text-white p-2 group-hover:bg-gray-300 group-hover:text-yellow-600">
+              Gifting
+            </h2>
+            <div className="p-2 hidden group-hover:block h-50 w-50 bg-gray-100 text-black absolute grid grid-cols-1 gap-4">
+              <h3 className="m-2">Corporate Gifts</h3>
+              <h3 className="m-2">Anniversary Gifts</h3>
+              <h3 className="m-2">Birthday Gifts</h3>
+              <h3 className="m-2">Gift Hampers</h3>
+              <h3 className="m-2">Personalized Gifts</h3>
+              <h3 className="m-2">Valentine's Day Special</h3>
+            </div>
+          </div>
+          <div className="group relative">
+            <h2 className="text-white group-hover:text-yellow-600 group-hover:bg-gray-300 p-2">
+              Art & Collectibles
+            </h2>
+            <div className="hidden absolute group-hover:block h-50 w-full bg-gray-100 text-black grid grid-cols-1 gap-4 text-center">
+              <h3 className="m-2">Sculpture</h3>
+              <h3 className="m-2">Masterpices</h3>
+            </div>
           </div>
         </div>
+      </nav>
+      <div className="h-45 w-full">
+        <img src="./images/home.png" className="h-full w-full"></img>
       </div>
     </div>
   );
