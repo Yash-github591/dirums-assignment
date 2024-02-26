b6ffd1cc175138d03bfe168b23c424a1ff200bf2
Yash-github591 <yashgargjsr1999@gmail.com>
2024-02-26 15:07:23 +0530

    almost completed with some bugs still remaining

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,21 +2,39 @@ import React, { useEffect, useState } from "react";
 import data from "../data";
 
 function Menu({ item }) {
-  const [cols, setCols] = useState(3);
   const [columnHeadings, setcolumnHeadings] = useState([]);
-  const [left, setLeft] = useState("");
 
   useEffect(() => {
-    const filteredData = data.filter((d) => d.parentId === item.id);
-    setcolumnHeadings(filteredData);
-    console.log(filteredData, filteredData.length);
-    if (filteredData.length > 3) {
-      setCols(6);
-      setLeft("left-0");
-    } else {
-      setCols(filteredData.length);
+    let filteredData = data.filter((d) => d.parentId === item.id);
+    for (let i = 0; i < filteredData.length; i++) {
+      filteredData[i].children = [];
     }
+
+    for (let i = 0; i < filteredData.length; i++) {
+      for (let j = 0; j < data.length; j++) {
+        if (filteredData[i].id === data[j].parentId) {
+          filteredData[i].children.push(data[j]);
+        }
+      }
+    }
+    setcolumnHeadings(filteredData);
   }, []);
+
+  var len = columnHeadings.length,
+    colsClass = "grid-cols-3",
+    left = "";
+
+  console.log("len=", len);
+
+  if (len > 3) {
+    colsClass = "grid-cols-6";
+    left = "left-0";
+  } else {
+    colsClass = `grid-cols-${len}`;
+    console.log("colsClass=", colsClass, typeof colsClass);
+  }
+
+  console.log(item.heading, columnHeadings);
 
   return (
     <div>
@@ -25,19 +43,17 @@ function Menu({ item }) {
           {item.heading}
         </h2>
         <div
-          className={`${left} gridbox opacity-0 scale-0 group-hover:opacity-100 shadow-lg mb-2 group-hover:scale-100 grid grid-cols-${cols} gap-2 p-4 bg-gray-100 text-black absolute`}
+          className={`${left} grid ${colsClass} gridbox opacity-0 scale-0 group-hover:opacity-100 shadow-lg mb-2 group-hover:scale-100 gap-2 p-4 bg-gray-100 text-black absolute`}
         >
           {columnHeadings.map((d) => {
             return (
               <div key={d.id}>
                 <h3 className="font-bold m-2 text-lg">{d.heading}</h3>
-                {data.map((curr) => {
+                {d.children.map((curr) => {
                   return (
-                    curr.parentId === d.id && (
-                      <h3 key={curr.id} className="m-2">
-                        {curr.heading}
-                      </h3>
-                    )
+                    <h3 key={curr.id} className="m-2">
+                      {curr.heading}
+                    </h3>
                   );
                 })}
               </div>
